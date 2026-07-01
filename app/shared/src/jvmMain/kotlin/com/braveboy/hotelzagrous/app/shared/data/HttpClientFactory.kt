32dd1e23cfcb56eb3532f446781c2b3c3de99d06
aminshojaei13 <m.aminshojaei@yahoo.com>
2026-07-01 17:51:49 +0330

    fix server address

--- app/shared/src/jvmMain/kotlin/com/braveboy/hotelzagrous/app/shared/data/HttpClientFactory.kt
+++ app/shared/src/jvmMain/kotlin/com/braveboy/hotelzagrous/app/shared/data/HttpClientFactory.kt
@@ -3,6 +3,8 @@ package com.braveboy.hotelzagrous.app.shared.data
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.java.Java
 
+const val localhost = "192.168.10.107"
+
 actual fun createHotelHttpClient(): HttpClient = HttpClient(Java)
 
-actual fun defaultApiBaseUrl(): String = "http://localhost:8090/api"
+actual fun defaultApiBaseUrl(): String = "http://$localhost:8090/api"
