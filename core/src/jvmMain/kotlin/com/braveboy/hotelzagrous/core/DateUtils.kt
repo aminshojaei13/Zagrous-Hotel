a60830297dcbb1f2ff1fd7d74e765a57c5a9898e
aminshojaei13 <m.aminshojaei@yahoo.com>
2026-07-19 16:04:54 +0330

    fix EVEN and ODD

--- core/src/jvmMain/kotlin/com/braveboy/hotelzagrous/core/DateUtils.kt
+++ core/src/jvmMain/kotlin/com/braveboy/hotelzagrous/core/DateUtils.kt
@@ -35,7 +35,8 @@ actual object DateUtils {
         if (parts.size != 3) return false
         return try {
             val pd = PersianDateTime(parts[0].toInt(), parts[1].toInt(), parts[2].toInt())
-            pd.day % 2 == 0
+            val dayOfWeek = pd.persianDayOfWeek().number
+            dayOfWeek % 2 != 0 && dayOfWeek != 7
         } catch (e: Exception) {
             false
         }
