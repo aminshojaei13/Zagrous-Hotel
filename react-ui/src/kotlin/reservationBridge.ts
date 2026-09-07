5a813ccc7a7f1e4d3132e0ad73c050778d2f575f
aminshojaei13 <m.aminshojaei@yahoo.com>
2026-09-08 01:36:09 +0330

    try step 6,7

--- react-ui/src/kotlin/reservationBridge.ts
+++ react-ui/src/kotlin/reservationBridge.ts
@@ -1,8 +1,11 @@
 import * as KotlinLibrary from 'HotelZagrous-app-shared';
 
-const reservationNamespace = KotlinLibrary.com.braveboy.hotelzagrous.app.shared.features.reservation;
+// @ts-ignore
+const Lib: any = KotlinLibrary.default || KotlinLibrary;
+
+const reservationNamespace = Lib.com.braveboy.hotelzagrous.app.shared.features.reservation;
 
 export const createReservationBridge = reservationNamespace.createReservationBridge;
 
-export type ReservationWebBridge = KotlinLibrary.com.braveboy.hotelzagrous.app.shared.features.reservation.ReservationWebBridge;
-export type ReservationStateJs = KotlinLibrary.com.braveboy.hotelzagrous.app.shared.features.reservation.ReservationStateJs;
+export type ReservationWebBridge = any;
+export type ReservationStateJs = any;
