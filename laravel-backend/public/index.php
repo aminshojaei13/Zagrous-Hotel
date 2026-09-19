97ce19c9f09923c2e6f2fd97980040159194875c
aminshojaei13 <m.aminshojaei@yahoo.com>
2026-09-19 22:05:54 +0330

    fix composer and php ERROR_DEPRECATED bug

--- laravel-backend/public/index.php
+++ laravel-backend/public/index.php
@@ -4,6 +4,10 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// Suppress deprecation warnings that break JSON responses
+error_reporting(E_ALL & ~E_DEPRECATED);
+ini_set('display_errors', '0');
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
